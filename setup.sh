28fc4cf7f96a3a534feef2cb3a5a3833f6838de2
Sunny Yuen <yuen5401@example.com>
2019-07-11 22:17:47 +1000

    Fixed unattened issue

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 #Add docker repository
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 #Installing docker
-sudo yum install docker-ce
+sudo yum -y install docker-ce
 #Adding current user to group
 sudo usermod -aG docker $(whoami)
 #Setting Docker to boot at start time
